b14a7ad078f0cfd4e95c50c8c9e9b23f49979ecc
sbrnag <nagendra.sbr@gmail.com>
2021-01-31 10:37:50 +0530

    eslint fix

--- utils/timeCheck.js
+++ utils/timeCheck.js
@@ -1,11 +1,9 @@
-const { logger } = require('./utils');
 module.exports.isCheckOutAllowed = (startTime, endTime) => {
   const currentDate = new Date();
   const currentTime = currentDate.getHours();
-  logger.debug(`currentTime : ${currentTime}, startTime: ${startTime}, endTime: ${endTime}`);
   if (currentTime > startTime && currentTime < endTime) {
     return true;
   }
-  // since deployed to heroku returning true. has to be false 
+  // since deployed to heroku returning true. has to be false
   return true;
 };
