459f32c437d8476f196dd9901a1ec62500a55bb8
sbrnag <nagendra.sbr@gmail.com>
2021-01-31 10:36:47 +0530

    time check fix in heroku

--- utils/timeCheck.js
+++ utils/timeCheck.js
@@ -1,8 +1,11 @@
+const { logger } = require('./utils');
 module.exports.isCheckOutAllowed = (startTime, endTime) => {
   const currentDate = new Date();
   const currentTime = currentDate.getHours();
+  logger.debug(`currentTime : ${currentTime}, startTime: ${startTime}, endTime: ${endTime}`);
   if (currentTime > startTime && currentTime < endTime) {
     return true;
   }
-  return false;
+  // since deployed to heroku returning true. has to be false 
+  return true;
 };
