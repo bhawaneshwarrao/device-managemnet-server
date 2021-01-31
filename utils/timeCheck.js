44d568dcb3f5d6a8e722927e2ab935c79ce462d4
sbrnag <nagendra.sbr@gmail.com>
2021-01-31 10:46:11 +0530

    fix

--- utils/timeCheck.js
+++ utils/timeCheck.js
@@ -3,7 +3,8 @@ module.exports.isCheckOutAllowed = (startTime, endTime) => {
   const currentTime = currentDate.getHours();
   if (currentTime > startTime && currentTime < endTime) {
     return true;
+  } else {
+    // since deployed to heroku returning true. has to be false
+    return true;
   }
-  // since deployed to heroku returning true. has to be false
-  return true;
 };
